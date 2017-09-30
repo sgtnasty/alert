35ac498c2d6dea79fd5145dbc7702284e4933fab
Ronaldo Nascimento <sgtnasty@gmail.com>
2017-09-30 16:26:04 -0400

    added GPL header

--- alert.py
+++ alert.py
@@ -2,6 +2,25 @@
 
 
 """
+alert.py - sends voice alerts to a phone via Twilio
+Copyright (C) 2017  Ronaldo Nascimento <sgtnasty@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
 Alert script for server notifications via Twilio
 
 https://www.twilio.com/docs/quickstart/python/
